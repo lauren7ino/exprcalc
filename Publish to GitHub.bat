3abc085da60cef17ff32af55c4b4f76aa9b20586
laurentino.martins <laurentino.martins@markdata.pt>
2017-09-01 14:20:43 +0100

    Keyup modified to Keydown

--- Publish to GitHub.bat
+++ Publish to GitHub.bat
@@ -1,11 +1,12 @@
 @echo off
 cls
-echo Pushing to GitHub 'master' and 'gh-pages'...
+echo Publishing to GitHub 'master' and 'gh-pages'...
 echo.
 
 "c:\Program Files\Git\bin\git.exe" push origin master
 echo.
 if errorlevel 1 pause
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://github.com/lauren7ino/exprcalc/tree/master
 
 ng build --prod --base-href "https://lauren7ino.github.io/exprcalc/"
 echo.
@@ -14,3 +15,4 @@ if errorlevel 1 pause
 angular-cli-ghpages
 echo.
 if errorlevel 1 pause
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://lauren7ino.github.io/exprcalc/
