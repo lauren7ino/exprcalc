859f8f82e4fd61f779b5a1644009430c6c723073
laurentino.martins <laurentino.martins@markdata.pt>
2017-09-07 15:32:46 +0100

    .

--- Publish to GitHub.bat
+++ Publish to GitHub.bat
@@ -1,18 +1,24 @@
 @echo off
 cls
-echo Publishing to GitHub 'master' and 'gh-pages'...
+echo Publishing to GitHub 'master' ...
 echo.
-
 git push origin master
 echo.
 if errorlevel 1 pause
 start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://github.com/lauren7ino/exprcalc/tree/master
 
+cls
+echo Compiling ...
+echo.
+echo [Type 'exit' on end to continue]
+echo.
 start /b /wait ng build --prod --base-href "https://lauren7ino.github.io/exprcalc/"
 echo.
-echo Type 'exit' to continue...
 if errorlevel 1 pause
 
+cls
+echo Publishing to GitHub 'gh-pages' ...
+echo.
 angular-cli-ghpages
 echo.
 if errorlevel 1 pause
