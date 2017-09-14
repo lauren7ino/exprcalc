92eb1f3f41131d92c7f8cd3d309b77a31be6d379
laurentino.martins <laurentino.martins@markdata.pt>
2017-09-14 15:51:22 +0100

    Tooptips added

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
+  @Input() prefix: string;
   @Input() label: string;
   @Input() extraClass: string;
   @Input() clipboardLabel: string;
